1ca3d74b2660550aebd9c680bccc8b4e88779f2f
jintaro.tanba <hnnskrhtnbsd2600@icloud.com>
2023-07-10 08:38:36 +0900

    mutate Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
-import { EditedProfile } from '@/interfaces/interfaces';
 import { useProfileQuery } from '@/hooks/useProfileQuery';
+import { useProfileMutate } from '@/hooks/useProfileMutate';
 import { useQueryClient } from 'react-query';
+import { useGlobalContext } from '@/context/ContextProvider';
 export const Profile = () => {
-  const [name,setName] = useState('')
-  const [favorite,setFavorite] = useState('');
-  const [avtar_url,setUrl] = useState('');
-  const profile = useProfileQuery();
+  const {editedProfile,update} = useGlobalContext()
+  const handleSubmit = async () => {
+    const {mutateProfile} = await useProfileMutate()
+    mutateProfile.mutate();
+  };
   return (
     <div className="">
-      <input className='bg-black my-2 px-3 py-2 text-sm focus:outline-none' value={'hello?'}/>
+      <input className='bg-black my-2 px-3 py-2 text-sm focus:outline-none' value={editedProfile.name} onChange={(e)=>{update({...editedProfile,name: e.target.value})}}/>
+      <input className='bg-black my-2 px-3 py-2 text-sm focus:outline-none' value={editedProfile.name} onChange={(e)=>{update({...editedProfile,avtar_url: e.target.value})}}/>
+      <h1 onClick={handleSubmit} className="">🫥</h1>
     </div>
   )
 }
